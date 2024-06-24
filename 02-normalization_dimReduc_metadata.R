1b77a768be5e8d3ed882e5ebee8c825d2c495818
Carolyn Brewster <carolynbrewster@gmail.com>
2024-06-24 11:22:39 -0500

    add color palettes

--- 02-normalization_dimReduc_metadata.R
+++ 02-normalization_dimReduc_metadata.R
@@ -18,9 +18,19 @@ nPCs <- list('undertissue'=55,'piwipos'=20)
 res <- list('undertissue'=1,'piwipos'=0.8)
 seeds <- list('undertissue'=42,'piwipos'=112358)
 
+
+# NOTE! "Parenchymal" was changed to "Secretory" in the
+# final mansucript to avoid confusion with other uses
+# of the term "parenchymal" in planarians.
 tissuelvs <- c("Phagocytic Cells","Epidermis","Intestine",
                "Muscle","Neural","Parenchymal","Pharynx",
                "Protonephridia","Stem Cells","Non-differentiated")
+
+tissueCols <- c("#b73000","#d6511c","#f48326",
+                "#fcb514","#8ac341","#279a2f","#03899b",
+                "#0256c9","#00131c","#b3b3b3")
+
+timeCols <- c("#D41159","#EEC900","#1E90FF")
 
 # ================= UNDER TISSUE ======================
 
@@ -46,7 +56,16 @@ umap <- as.matrix(meta[Cells(seu),c('global_umap_1','global_umap_2')])
 colnames(umap) <- c('umap_1','umap_2')
 seu[['umap']] <- CreateDimReducObject(umap)
 
+# color palettes!
+seu@misc$colors <- list()
+seu@misc$colors$tissue <- setNames(tissueCols,levels(seu$tissue))
+seu@misc$colors$timepoint <- setNames(timeCols,levels(seu$timepoint))
 saveRDS(seu,'../data/seurat_objects/under_tissue.rds')
+
+# DID YOU KNOW: you can save basically whatever you want in
+# the Seurat object's misc slot? It's super handy! Never
+# misplace your color palettes, plotting parameters, or
+# handy helper functions again!
 
 # ================= PIWI1 POSITIVE SUBSET ============
 
@@ -62,4 +81,10 @@ seu$tissue <- factor(meta[Cells(seu),'tissue'],levels=tissuelvs)
 umap <- as.matrix(meta[Cells(seu),c('stem_cell_umap_1','stem_cell_umap_2')])
 colnames(umap) <- c('umap_1','umap_2')
 seu[['umap']] <- CreateDimReducObject(umap)
+
+# color palettes!
+seu@misc$colors <- list()
+seu@misc$colors$tissue <- setNames(tissueCols,levels(seu$tissue))
+seu@misc$colors$timepoint <- setNames(timeCols,levels(seu$timepoint))
+
 saveRDS(seu,'../data/seurat_objects/piwi1_positive_subset.rds')
